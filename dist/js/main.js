ff8bbb56b2bfdb3e9a60655d3dc92049f4e441e8
ikorobka <ikorobka009@gmail.com>
2018-01-05 00:20:15 +0200

    header sass media, js

---
+++ dist/js/main.js
@@ -0,0 +1,14 @@
+function Navigation(t) {
+  this.btn = document.getElementById(t.btnId), this.nav = document.getElementById(t.navigationId), this.overlay = document.createElement("div"), this.openNav_ = () => {
+    document.body.appendChild(this.overlay), setTimeout(() => this.nav.classList.add(t.activeNavClass), 200);
+  }, this.hideNav_ = e => {
+    (document.querySelector(`.${t.overlayClass}`) && 27 === e.keyCode || "click" === e.type || "dragend" === e.type) && (this.nav.classList.remove(t.activeNavClass), this.nav.hasAttribute("style") && this.nav.removeAttribute("style"), setTimeout(() => document.body.removeChild(this.overlay), 200));
+  }, this.dragStart_ = t => {
+    let e = 270 - t.screenX;e < 270 && (this.nav.style.transform = `translateX(${-e}px)`);
+  }, this.events_ = () => {
+    this.btn.addEventListener("click", this.openNav_), this.overlay.addEventListener("click", this.hideNav_), window.addEventListener("keyup", this.hideNav_), this.nav.addEventListener("drag", this.dragStart_), this.nav.addEventListener("dragend", this.hideNav_);
+  }, this.init_ = () => {
+    this.events_(), this.overlay.classList.add(t.overlayClass), console.log(t.navigationId);
+  }, this.init_();
+}const nav = { btnId: "checkbox", navigationId: "nav", activeNavClass: "nav--active", overlayClass: "overlay" },
+      offCanvas = new Navigation(nav);
